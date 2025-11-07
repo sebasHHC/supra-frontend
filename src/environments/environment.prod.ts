9cbcb58ae8728dc019622afa102afb236e19c1fc
Sebastian Gonzalez <sebastian.magm@gmail.com>
2025-11-07 13:04:02 -0300

    agregamos api de nuevo de render a prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api' // ✅ solo para desarrollo local
+  apiUrl: 'https://supra-backend-30hh.onrender.com/api' // ✅ solo para desarrollo local
 };
